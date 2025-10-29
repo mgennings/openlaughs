abf7f55b98eab730f840a2e2677e0dd1a83c5da1
Gennings, Matt <matt.gennings@yunextraffic.com>
2025-10-29 10:07:26 -0500

    feat: update amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,20 +1,20 @@
 export type AmplifyDependentResourcesAttributes = {
-  api: {
-    openlaughs: {
-      GraphQLAPIEndpointOutput: 'string';
-      GraphQLAPIIdOutput: 'string';
-      GraphQLAPIKeyOutput: 'string';
-    };
-  };
-  auth: {
-    openlaughs0fb0c848: {
-      AppClientID: 'string';
-      AppClientIDWeb: 'string';
-      IdentityPoolId: 'string';
-      IdentityPoolName: 'string';
-      UserPoolArn: 'string';
-      UserPoolId: 'string';
-      UserPoolName: 'string';
-    };
-  };
-};
+  "api": {
+    "openlaughs": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "openlaughs0fb0c848": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
